8c63842ee084a5e1c2c212cf896979d72acd09f2
Vast342 <jcpasfield@gmail.com>
2023-07-21 19:10:23 -0400

    Initializing my other Bot
    I accidentally deleted what I had had when I cloned the repo again and so now I have to recreate it and the planning.

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,19 @@
 ﻿using ChessChallenge.API;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Linq;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        var rng = new Random();
+        List<Move> moves = board.GetLegalMoves().ToList();
+        
+
+
+
+        return moves[rng.Next(moves.Count-1)];
     }
 }
